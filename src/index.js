3addf1971d70fb52242db339b8c76efdf55506df
Josue <jecheverria@estudiantec.cr>
2024-10-09 00:52:41 -0600

    Arreglo con imagenes y estilos de css

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import NavBar from "./components/Navbar";
+import HeaderImage from './img/header.jpg';
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
       <BrowserRouter>
+        <img src={HeaderImage} alt="Insitituto Tecnologico de Costa Rica Escuela de computacion Unidad de Administracion de laboratorios" />
         <NavBar />
         <App />
       </BrowserRouter>
