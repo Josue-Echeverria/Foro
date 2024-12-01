75b4104a9ed5e63cda40d91181f040fdefd139f3
Josue <jecheverria@estudiantec.cr>
2024-12-01 17:55:21 -0600

    security fix

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzHXDLwNVJsBvSVCUOJrMG6tegPNivRbw",
-  authDomain: "ingenieria-en-computacion.firebaseapp.com",
-  projectId: "ingenieria-en-computacion",
-  storageBucket: "ingenieria-en-computacion.firebasestorage.app",
-  messagingSenderId: "64503692317",
-  appId: "1:64503692317:web:f75727aeaa9477b7349af4",
-  measurementId: "G-RNLV29BTFH"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
